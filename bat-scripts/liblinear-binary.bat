036b6c1b4dadcbca4943e0ee0e1b8eff7b70e9ef
Mihail <misha.kukuev@gmail.com>
2017-06-10 16:41:02 +0300

    bat-scripts modified

--- bat-scripts/liblinear-binary.bat
+++ bat-scripts/liblinear-binary.bat
@@ -1,7 +1,7 @@
 spark-submit --class liblinear.svm.LiblinearBinary ^
- --master local[4] ^
+ --master local[8] ^
  --jars D:\Google-drive\Thesis\program\spark-svm\liblinear\build\svm-liblinear.jar,D:\Google-drive\Thesis\program\spark-svm\mllib\build\svm-mllib.jar, D:\tmp\spark-liblinear-1.96.jar ^
- D:\Study\univer\thesis\libraries\datasets\rcv1_train.binary ^
  D:\Study\univer\thesis\libraries\datasets\rcv1_test.binary ^
+ D:\Study\univer\thesis\libraries\datasets\rcv1_train.binary ^
  D:\tmp\results_liblinear.txt
 pause
--- bat-scripts/mllib-binary.bat
+++ bat-scripts/mllib-binary.bat
@@ -1,8 +1,8 @@
 spark-submit --class mllib.svm.MllibBinary ^
  --master local[4] ^
- D:\Google-drive\Thesis\program\spark-svm\mllib\build\svm-mllib.jar ^
+ D:/Google-drive/Thesis/program/spark-svm/mllib/build\svm-mllib.jar ^
  D:/Study/univer/thesis/libraries/datasets/rcv1_train.binary ^
  D:/Study/univer/thesis/libraries/datasets/rcv1_test.binary ^
  D:/tmp/results_mllib.txt ^
- 1
+ 3
 pause
